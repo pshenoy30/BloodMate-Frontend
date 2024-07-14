f1af0d2185d96fccab21fa6d2b8ec51b472d2a44
Pratiksha Shenoy <pratikshashenoy@Pratikshas-MacBook-Pro.local>
2024-07-15 05:23:25 +0530

    Added the not-found page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import "./NotFoundPage.scss";
+import {Link} from "react-router-dom";
 
 export default function NotFoundPage() {
   return (
-    <div>NotFoundPage</div>
+    <section className="not-found">
+      <article className="not-found__container">
+        <h1 className="not-found__title">404</h1>
+        <h3 className="not-found__sub-title">OH NO!!!! It seems like you are lost??</h3>
+        <h3 className="not-found__sub-title">Click on the button below to go back to the home page</h3>
+        <Link to="/">
+          <button type="button" className="not-found__button">Home</button>
+        </Link>
+      </article>
+    </section>
   )
 }
